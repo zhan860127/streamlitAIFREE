cfdcfdf33769f814ef96fe1f15c9d666745f1a3b
tonyzhan <tonyzhan@vgame.com.tw>
2022-09-27 17:29:15 +0800

    add something

---
+++ test_streamlit.py
@@ -0,0 +1,15 @@
+import sys
+from streamlit.web import cli as stcli
+import cv2 as cv
+from main import analyzeses
+from PIL import Image
+import numpy as np
+
+def test_streamlit():
+    print("hello")
+
+def test_AImodel():
+    a =  np.array(Image.open("./test_sample./Sample_01_B.jpg"))
+    b =  np.array(Image.open("./test_sample./Sample_01_D.jpg"))
+    analyzeses(a,b)
+
