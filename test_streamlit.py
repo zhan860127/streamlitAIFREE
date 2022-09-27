ca522f162bbdfc925bbaadc25e62014045fa5128
tonyzhan <tonyzhan@vgame.com.tw>
2022-09-27 17:44:47 +0800

    modify path

--- test_streamlit.py
+++ test_streamlit.py
@@ -4,12 +4,14 @@ import cv2 as cv
 from main import analyzeses
 from PIL import Image
 import numpy as np
+import os
 
 def test_streamlit():
     print("hello")
 
 def test_AImodel():
-    a =  np.array(Image.open(r".//test_sample.//Sample_01_B.jpg"))
-    b =  np.array(Image.open(r".//test_sample.//Sample_01_D.jpg"))
+    a =  np.array(Image.open(r".\\test_sample.\\Sample_01_B.jpg"))
+    b =  np.array(Image.open(r".\\test_sample.\\Sample_01_D.jpg"))
+    print(os.path.isfile(r".\\test_sample.\\Sample_01_B.jpg"))
     analyzeses(a,b)
 
